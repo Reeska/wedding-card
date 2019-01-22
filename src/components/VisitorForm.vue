881d270c1dcb3ba5384a2c95ed41d744c58a225e
clementine-chasles <clementine.chasles@gmail.com>
2019-01-22 16:08:40 +0100

    Update VisitorForm.vue

--- src/components/VisitorForm.vue
+++ src/components/VisitorForm.vue
@@ -1,22 +1,40 @@
 <template>
   <div>
-    <v-form @submit.prevent="addVisitor()">
+    <v-form class="col-lg-4 offset-lg-4" @submit.prevent="addVisitor()">
       <div v-if="step === 1">
         <v-text-field
-          color="red"
           v-model="firstname"
-          label="Votre prénom">
+          label="Votre prénom"
+          color="#300924">
         </v-text-field>
-        <button :disabled="!firstname" type="button" @click="goToSecondStep()">Continuer</button>
+        <v-btn :disabled="!firstname" type="button" @click="goToSecondStep()">Continuer</v-btn>
       </div>
-      <div v-else>
-        <div>Mairie ?: <input type="checkbox" v-model="cityhall"></div>
-        <div v-show="cityhall">Accompagnants : <input type="number" v-model="cityhall_companions"></div>
-        <div>Bar ?: <input type="checkbox" v-model="bar"></div>
-        <div v-show="bar">Accompagnants : <input type="number" v-model="bar_companions"></div>
-        <button type="submit">Enregistrer</button>
+      <div v-if="step === 2">
+        <v-checkbox label="Je viens à la mairie" v-model="cityhall" color="#300924"></v-checkbox>
+        <div v-show="cityhall">
+          <v-text-field
+            v-model="cityhall_companions"
+            label="Nombre d'accompagnants à la mairie"
+            type="number"
+            color="#300924">
+          </v-text-field>
+        </div>
+        <v-checkbox label="Je viens aux P'tites poules" v-model="bar" color="#300924"></v-checkbox>
+        <div v-show="bar">
+          <v-text-field
+            v-model="bar_companions"
+            label="Nombre d'accompagnants au bar"
+            type="number"
+            color="#300924">
+          </v-text-field>
+        </div>
+        <v-btn type="submit">Enregistrer</v-btn>
       </div>
     </v-form>
+
+    <div v-if="step === 3">
+      Merci :)
+    </div>
 
     <ul>
       <li v-for="user in users" :key="user.id">{{ user.id }} - {{ user.firstname }}</li>
@@ -35,9 +53,9 @@
         users: [],
         firstname: '',
         cityhall: false,
-        cityhall_companions: 1,
+        cityhall_companions: 0,
         bar: false,
-        bar_companions: 1,
+        bar_companions: 0,
         step: 1,
       };
     },
@@ -64,14 +82,18 @@
       },
 
       async addVisitor() {
-        await visitorsRef.add({
-          firstname: this.firstname,
-          cityhall: this.cityhall,
-          cityhall_companions: this.cityhall_companions,
-          bar: this.bar,
-          bar_companions: this.bar_companions,
-        });
-
+        try {
+          await visitorsRef.add({
+            firstname: this.firstname,
+            cityhall: this.cityhall,
+            cityhall_companions: this.cityhall_companions,
+            bar: this.bar,
+            bar_companions: this.bar_companions,
+          });
+        } catch (error) {
+          console.log('error', error);
+        }
+        this.step = 3;
         this.loadUsers();
       },
     },

8f9727199b58c3303f16601e65d184c2ea2f95d8
clementine-chasles <clementine.chasles@gmail.com>
2019-01-22 13:44:53 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import App from './App.vue';
 import vueSmoothScroll from 'vue2-smooth-scroll';
 import './filters';
 import BootstrapVue from 'bootstrap-vue';
+import Vuetify from 'vuetify'
 
 Vue.use(vueSmoothScroll);
 Vue.use(BootstrapVue);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 

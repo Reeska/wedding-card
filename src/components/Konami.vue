4a9b8a7c8809a9d971fc54016a763ce0bb790528
clementine-chasles <clementine.chasles@gmail.com>
2019-01-28 14:08:05 +0100

    Update Konami.vue

--- src/components/Konami.vue
+++ src/components/Konami.vue
@@ -7,39 +7,42 @@
 
   @Component
   export default class Konami extends Vue implements OnMounted {
+
+    private const allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+        65: 'a',
+        66: 'b',
+    };
+
+    private const konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+    private konamiCodePosition = 0;
+
     public mounted() {
-        const allowedKeys = {
-            37: 'left',
-            38: 'up',
-            39: 'right',
-            40: 'down',
-            65: 'a',
-            66: 'b',
-        };
+        document.addEventListener('keydown', this.onKeyDown);
+    }
 
-        // the 'official' Konami Code sequence
-        const konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+    public onKeyDown(e) {
+        // get the value of the key code from the key map
+        var key = this.allowedKeys[e.keyCode];
+        // get the value of the required key from the konami code
+        var requiredKey = this.konamiCode[this.konamiCodePosition];
 
-        var konamiCodePosition = 0;
-        document.addEventListener('keydown', function(e) {
-            // get the value of the key code from the key map
-            var key = allowedKeys[e.keyCode];
-            // get the value of the required key from the konami code
-            var requiredKey = konamiCode[konamiCodePosition];
-
-            // compare the key with the required key
-            if (key == requiredKey) {
-                // move to the next key in the konami code sequence
-                konamiCodePosition++;
-                // if the last key is reached, activate cheats
-                if (konamiCodePosition == konamiCode.length) {
-                console.log('konami !!!!');
-                konamiCodePosition = 0;
-                }
-            } else {
-                konamiCodePosition = 0;
+        // compare the key with the required key
+        if (key == requiredKey) {
+            // move to the next key in the konami code sequence
+            this.konamiCodePosition++;
+            // if the last key is reached, activate cheats
+            if (this.konamiCodePosition == this.konamiCode.length) {
+            console.log('konami !!!!');
+            this.konamiCodePosition = 0;
             }
-        });
+        } else {
+            this.konamiCodePosition = 0;
+        }
     }
   }
 </script>

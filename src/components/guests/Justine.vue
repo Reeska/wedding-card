ad0bba67a890793cdd3032dc09e363c7e2442449
clementine-chasles <clementine.chasles@gmail.com>
2019-01-31 16:52:58 +0100

    Add files via upload

---
+++ src/components/guests/Justine.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Maman ! <img src="../../assets/cute.gif"/></p>
+    <p><img src="../../assets/hug.gif" class="verySmall"/></p>
+    <p>Je sais que je suis ta fille préférée, t'en fais pas, tu n'as pas besoin de le dire.</p>
+    <p><img src="../../assets/bean.gif" class="smallGif"/></p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Justine extends Vue {}
+</script>
+
+<style lang="scss">
+  .smallGif {
+    max-width: 290px;
+    margin-bottom: 10px;
+  }
+
+  .verySmall {
+    max-width: 200px;
+  }
+</style>

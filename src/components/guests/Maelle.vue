418f1f2b398b333651d7e24f56a0aa55e37bc6d6
clementine-chasles <clementine.chasles@gmail.com>
2019-01-31 16:46:17 +0100

    Add files via upload

---
+++ src/components/guests/Maelle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Ça va être la fête <img src="../../assets/sheep.gif"/></p>
+    <p><img src="../../assets/mammamia.gif" class="smallGif"/></p>
+    <p>Par contre si tu me spoiles encore une fois sur l'assassin royal...</p>
+    <p><img src="../../assets/chick.jpg" class="verySmall"/></p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Maelle extends Vue {}
+</script>
+
+<style lang="scss">
+  .smallGif {
+    max-width: 290px;
+    margin-bottom: 10px;
+  }
+
+  .verySmall {
+    max-width: 200px;
+  }
+</style>

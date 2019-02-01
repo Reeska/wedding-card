3f22aa13aeb0ca83fcdbf2ff67aeab86a75bbb78
clementine-chasles <clementine.chasles@gmail.com>
2019-02-01 13:57:49 +0100

    Add files via upload

---
+++ src/components/guests/Catherine.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div><img src="../../assets/sherlock.gif" class="smallGif"/></div>
+    <p>Un petit gif de Sherlock pour te faire plaisir <img src="../../assets/proud.gif"/></p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Catherine extends Vue {}
+</script>
+
+<style lang="scss">
+  .smallGif {
+    max-width: 290px;
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/components/guests/Thierry.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div><img src="../../assets/ricard.gif" class="smallGif"/></div>
+    <p>On en ramènera une bouteille au bar, t'en fais pas <img src="../../assets/proud.gif"/></p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Thierry extends Vue {}
+</script>
+
+<style lang="scss">
+  .smallGif {
+    max-width: 290px;
+    margin-bottom: 10px;
+  }
+</style>

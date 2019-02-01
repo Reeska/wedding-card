9f59acab6873fec87c402aa263777c56cb3203a6
clementine-chasles <clementine.chasles@gmail.com>
2019-02-01 14:25:41 +0100

    Add files via upload

---
+++ src/components/guests/Sabine.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p><img src="../../assets/party_hard.gif" class="smallGif"/></p>
+    <p>Bon essaye d'arriver à l'heure à ce mariage là s'teuplait <img src="../../assets/roll.gif"/><br/>
+    Et de pas me draguer... devant Thomas <img src="../../assets/hehe.gif"/></p>
+    <p><img src="../../assets/sheep.gif"/></p>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Sabine extends Vue {}
+</script>
+
+<style lang="scss">
+  .smallGif {
+    max-width: 290px;
+    margin-bottom: 10px;
+  }
+
+  .verySmall {
+    max-width: 200px;
+  }
+</style>
